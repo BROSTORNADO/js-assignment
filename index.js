ee1582b2a9eae9e8a862a63c7a5e4d5e365aabe6
Nahuel Alexis Balsas Faravelli <122588119+Nahuel149@users.noreply.github.com>
2023-08-07 16:13:23 +0900

    Changes described in group chat

--- index.js
+++ index.js
@@ -1,67 +1,132 @@
 function computerPlay() {
-  const options = ['Rock', 'Paper', 'Scissors']
-
-  return options[Math.floor(Math.random() * 3)]
+    const options = ['Rock', 'Paper', 'Scissors'];
+    return options[Math.floor(Math.random() * options.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
-  const playerSel = playerSelection.toLowerCase()
-  const computerSel = computerSelection.toLowerCase()
-  console.log(`you : ${playerSel}`)
-  console.log(`computer : ${computerSel}`)
+    const formattedPlayerSelection = playerSelection.toLowerCase();
+    const formattedComputerSelection = computerSelection.toLowerCase();
 
-  switch (playerSel) {
-    case computerSel:
-      return "It's a tie!"
-    case 'rock':
-      return computerSel === 'scissors'
-        ? 'You win, Rock beats Scissors'
-        : 'You lose, Rock are beaten by Paper '
-    case 'scissors':
-      return computerSel === 'paper'
-        ? 'You win, Scissors beat Paper'
-        : 'You lose, Scissors are beaten by Rock'
-    case 'paper':
-      return computerSel === 'rock'
-        ? 'You win, Paper beats Rock'
-        : 'You lose, Paper is beaten by Scissors'
-    default:
-      return 'Invalid choice!'
-  }
+    console.log(`You : ${formattedPlayerSelection}`);
+
+    switch (formattedPlayerSelection) {
+        case formattedComputerSelection:
+            return "It's a draw!";
+        case 'rock':
+            return formattedComputerSelection === 'scissors'
+                ? 'You win, Rock beats Scissors'
+                : 'You lose, Rock is beaten by Paper';
+        case 'scissors':
+            return formattedComputerSelection === 'paper'
+                ? 'You win, Scissors beat Paper'
+                : 'You lose, Scissors are beaten by Rock';
+        case 'paper':
+            return formattedComputerSelection === 'rock'
+                ? 'You win, Paper beats Rock'
+                : 'You lose, Paper is beaten by Scissors';
+        default:
+            return 'Invalid choice!';
+    }
+}
+
+function displayWelcomeMessage() {
+    alert("WELCOME TO ROCK, PAPER, SCISSORS!\n\nThe best classic game!\n\nPlease first press F12 to open your console");
+    alert("Here's how it works:\n\nRock beats Scissors\nScissors beats Paper\nPaper beats Rock\n\nLet's Go!");
+}
+
+function getPlayerSelection(round) {
+    let validSelection = false;
+    let playerSelection;
+
+    while (!validSelection) {
+        playerSelection = prompt(`Round ${round}: Choose Rock, Paper, or Scissors`);
+
+        if (playerSelection === null) {
+            console.log('Game canceled.');
+            return null;  // Handle player canceling the game
+        }
+
+        if (["rock", "paper", "scissors"].includes(playerSelection.toLowerCase())) {
+            validSelection = true;
+        } else {
+            console.log("Be careful with your fat fingers! Please choose Rock, Paper, or Scissors.");
+        }
+    }
+
+    return playerSelection;
+}
+
+function determineWinner(playerScore, computerScore) {
+    if (playerScore > computerScore) {
+        return "You are unstoppable! You won this game!";
+    } else if (playerScore < computerScore) {
+        return "You are a loser! Try to play something else!";
+    } else {
+        return "It's a draw. Play it again to prove you're good!";
+    }
+}
+
+function promptPlayAgain(playerScore, computerScore) {
+    if (playerScore > computerScore) {
+        return prompt("Congrats! You are so good, but you think you can win again? (yes or no)");
+    } else {
+        return prompt("Do you have the courage to play again, chicken? (yes or no)");
+    }
+}
+
+function promptChickenConfirmation() {
+    return prompt("So you are a chicken? (yes or no)");
 }
 
 const game = () => {
-  const times_won = {
-    you: 0,
-    computer: 0,
-  }
-  for (let i = 0; i < 5; i++) {
-    let player = prompt(`Round ${i + 1} : Rock, paper, or scissors, let the hilarity commence! 🤝🤣`)
-    let computerP = computerPlay()
-    let result = playRound(player, computerP)
-    if (result.charAt(4) === 'w') {
-      times_won.you += 1
-    } else if (result.charAt(4) === 'l') {
-      times_won.computer += 1
+    displayWelcomeMessage();
+    let playAgain = true;
+
+    while (playAgain) {
+        let playerScore = 0;
+        let computerScore = 0;
+
+        for (let round = 1; round <= 5; round++) {
+            let playerSelection = getPlayerSelection(round);
+
+            if (playerSelection === null) {
+                playAgain = false;
+                break;
+            }
+
+            const computerSelection = computerPlay();
+
+            console.log('Computer plays: ' + computerSelection);
+            const result = playRound(playerSelection, computerSelection);
+            console.log(result);
+
+            if (result.startsWith('You win')) {
+                playerScore++;
+            } else if (result.startsWith('You lose')) {
+                computerScore++;
+            }
+        }
+
+        console.log(`Final Scores:\nPlayer: ${playerScore}\nComputer: ${computerScore}`);
+
+        const endMessage = determineWinner(playerScore, computerScore);
+        console.log(endMessage);
+
+        const playAgainResponse = promptPlayAgain(playerScore, computerScore);
+
+        if (playAgainResponse === null || playAgainResponse.toLowerCase() !== "yes") {
+            const confirmResponse = promptChickenConfirmation();
+
+            if (confirmResponse === null || confirmResponse.toLowerCase() === "yes") {
+                console.log("Oh I see, so you are a real chicken then, give this game away to a real gamer!");
+                playAgain = false;
+            } else {
+                playAgain = true;
+            }
+        }
+
+        console.clear();
     }
-    console.log(result)
-  }
+};
 
-  const { you, computer } = times_won
-  console.log(`Score : `, you, computer)
-  if (you === computer) {
-    console.log("It's a tie, no winners, just friends in a comedic standoff! 😄🤝")
-  } else if (you > computer) {
-    console.log('You rock! 🎉🏆 Victory is yours! 👑😄 ')
-  } else {
-    console.log('Computer wins! 🎮💻 Better luck next time! 😄🤝')
-  }
-  const replay = prompt(`Do you wanna play again ? (yes or no) `)
-  if (replay.toLowerCase() == 'yes'){
-  console.clear()
-  game()
-}
-    
-}
-
-game()
+game();
